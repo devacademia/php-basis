9368344bb71facef1c709ff41ae747640e549b60
SilverD3 <mrboornesilver@gmail.com>
2023-06-26 07:56:27 +0100

    Super global Variables

---
+++ superglobales.php
@@ -0,0 +1,50 @@
+<?php
+
+$number = 10;
+
+// Superglobal $GLOBALS
+echo '$_GLOBALS <br>';
+print_r($GLOBALS);
+
+echo '<br><br>';
+
+// Superglobal $_SERVER
+echo '$_SERVER <br>';
+print_r($_SERVER);
+
+echo '<br><br>';
+
+// Superglobal $_REQUEST
+print_r($_REQUEST);
+
+echo '<br><br>';
+
+// Superglobal $_POST
+print_r($_POST);
+
+echo '<br><br>';
+
+// Superglobal $_GET
+print_r($_GET);
+
+echo '<br><br>';
+
+// Superglobal $_FILES
+print_r($_FILES);
+
+echo '<br><br>';
+
+// Superglobal $_ENV
+print_r($_ENV);
+
+echo '<br><br>';
+
+// Superglobal $_COOKIE
+print_r($_COOKIE);
+
+echo '<br><br>';
+
+// Superglobal $_SESSION
+print_r($_SESSION);
+
+
